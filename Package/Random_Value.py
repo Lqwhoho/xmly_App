89459b0c7580a7b7738b0b366c49eab7448f5a82
lqwhoho <45266347@qq.com>
2019-08-23 17:54:18 +0800

    消除警示语 增加获取随机数字+字母的方法

--- Package/Random_Value.py
+++ Package/Random_Value.py
@@ -8,13 +8,37 @@ import string
 
 
 def random_char(l):
-    return ''.join(random.sample
-                   (string.ascii_lowercase, l))
+    return ''.join(random.sample(string.ascii_lowercase, l))
+
+
+'''生成随机指定位数的整数数字'''
+
+
+def random_integer(l):
+    return ''.join(str(i) for i in random.sample(range(0, 9), l))
 
 
 '''获取固定长度随机字符串(字母+数字)'''
 
 
 def random_char_number(l):
-    random_value = ''.join(random.sample(string.ascii_letters + string.digits, l))
-    return random_value
+    return ''.join(random.sample(string.ascii_letters + string.digits, l))
+
+
+'''
+def random_char_number(l):
+    n = random.randint(1, 4)     # 数字个数1~4之间
+    c = l - n
+    print(n, c)
+
+    list_n = '1234567890'
+    list_c = 'abcdefghigklmnopqrstuvwxyz'
+    s = ""                # 保存随机码
+    for x in range(n):
+        s += random.choice(list_n)  # 随机数字
+    for x in range(c):
+        s += random.choice(list_c)  # 随机字符
+    return s                       # 返回随机码
+'''
+
+
--- Package/__init__.py
+++ Package/__init__.py
@@ -1,2 +1,2 @@
 __all__ = ['Element_Existence_IF', 'Get_Screenshot', 'Judging_Element_Existence', 'Judging_Toast', 'location',
-           'Relative_Coordinate_Click', 'Swipe', 'Wait_Element', 'Read_Excel_Data']
+           'Relative_Coordinate_Click', 'Swipe', 'Wait_Element', 'Read_Excel_Data', 'Random_Value']
